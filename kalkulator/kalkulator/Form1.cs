216a95c9c3659ce1ba2e56848ad4afd4e21670d9
polinrez <rezanova96@mail.ru>
2016-02-25 14:50:48 +0600

    Last changes

--- kalkulator/kalkulator/Form1.cs
+++ kalkulator/kalkulator/Form1.cs
@@ -16,5 +16,47 @@ namespace kalkulator
         {
             InitializeComponent();
         }
+
+        //private double firstArgument, secondArgument;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double firstArgument = Convert.ToDouble(textBox1.Text);
+            double secondArgument = Convert.ToDouble(textBox2.Text);
+            double resultSumm = firstArgument - secondArgument;
+            label2.Text = resultSumm.ToString();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double firstArgument = Convert.ToDouble(textBox1.Text);
+            double secondArgument = Convert.ToDouble(textBox2.Text);
+            double resultSumm = firstArgument+secondArgument;
+            label2.Text = resultSumm.ToString();
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double firstArgument = Convert.ToDouble(textBox1.Text);
+            double secondArgument = Convert.ToDouble(textBox2.Text);
+            double resultSumm = firstArgument * secondArgument;
+            label2.Text = resultSumm.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double firstArgument = Convert.ToDouble(textBox1.Text);
+            double secondArgument = Convert.ToDouble(textBox2.Text);
+            double resultSumm = firstArgument / secondArgument;
+            label2.Text = resultSumm.ToString();
+        }
+
     }
 }
