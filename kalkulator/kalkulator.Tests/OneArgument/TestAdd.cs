a3c046dada8de69b9c888c81e4e2bc226dc13d44
polinrez <rezanova96@mail.ru>
2016-03-24 14:43:52 +0600

    add test

---
+++ kalkulator/kalkulator.Tests/OneArgument/TestAdd.cs
@@ -0,0 +1,19 @@
+﻿using kalkulator.TwoArgument;
+using NUnit.Framework;
+
+namespace kalkulator.Tests.OneArgument
+{
+    [TestFixture]
+    public class TestAdd
+    {
+        [Test]
+        public void AddTest()
+        {
+            Add calck = new Add();
+            double res = calck.Calculate(2,2);
+            Assert.AreEqual(4, res);
+            res = calck.Calculate(1, 8);
+            Assert.AreEqual(9, res);
+        }
+    }
+}
